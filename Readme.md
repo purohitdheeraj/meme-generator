a78911f31be6094958da6670aa5bd29dfac14a47
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2023-02-08 17:23:33 +0530

    Chores: Refactor Meme Component

--- Readme.md
+++ Readme.md
@@ -1,7 +1,37 @@
 ## Meme-Generator
 
--   [X] Design
--   [ ] Events 
+-   [x] Design
+-   [ ] Events
 -   [ ] States
 -   [ ] Side Effect
 -   [ ] Forms
+
+# props vs state
+
+1. How would you describe the concept of "state"?
+   A way for React to remember saved values from within a component.
+   This is similar to declaring variables from within a component,
+   with a few added bonuses (which we'll get to later)
+
+2. When would you want to use props instead of state?
+   Anytime you want to pass data into a component so that
+   component can determine what will get displayed on the
+   screen.
+
+3. When would you want to use state instead of props?
+   Anytime you want a component to maintain some values from
+   within the component. (And "remember" those values even
+   when React re-renders the component).
+
+4. What does "immutable" mean? Are props immutable? Is state immutable?
+   Unchanging. Props are immutable. State is mutable.
+
+5. Functional State Update
+    - Note: if you ever need the old value of state
+    - to help you determine the new value of state,
+    - you should pass a callback function to your
+    - state setter function instead of using
+    - state directly. This callback function will
+    - receive the old value of state as its parameter,
+    - which you can then use to determine your new
+    - value of state.
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,23 +1,69 @@
 import React from "react";
+import memesData from "../memesData.js";
 
 function Meme() {
+	// * STATE : here our image is variable
+	// * every time it will change on btn click
+	// const [memeImage, setMemeImage] = React.useState("");
+
+	// complex state : object
+	// refactor
+
+	const [meme, setMeme] = React.useState({
+		topText: "",
+		bottomText: "",
+		randomImage: "http://i.imgflip.com/1bij.jpg",
+	});
+
+	const [memeImages, setMemeImages] =
+		React.useState(memesData);
+
+	function getMemeImage(e) {
+		e.preventDefault();
+
+		const memesArr = memeImages.data.memes;
+
+		const randomNum = Math.floor(
+			Math.random() * memesArr.length
+		);
+
+		const imageUrl = memesArr[randomNum].url;
+
+		setMeme((prev) => ({
+			...prev,
+			randomImage: imageUrl,
+		}));
+	}
+
 	return (
-		<main>
+		<main className="main--wrapper">
 			<form className="form">
 				<input
 					className="form--input"
 					type="text"
-					placeholder="text 1"
+					placeholder="top text"
 				/>
 				<input
 					className="form--input"
 					type="text"
-					placeholder="text 2"
+					placeholder="bottom text"
 				/>
-				<button className="btn form--btn">
+				<button
+					onClick={getMemeImage}
+					className="btn form--btn"
+				>
 					Get a new meme image 🖼
 				</button>
 			</form>
+			<div className="display--image">
+				{meme.randomImage && (
+					<img
+						className="meme--image"
+						src={meme.randomImage}
+						alt="a meme"
+					/>
+				)}
+			</div>
 		</main>
 	);
 }
